c5a4c169d7788e212b49baf692dc0cf6e1e15beb
Shex003 <eng.osman003@gmail.com>
2024-10-01 13:07:36 +0200

    settingUpANext.jsProject-creatinRoutsAndNavigatingBetweenPages-creatingLayout-addingInteractivityWithClientComponent

---
+++ app/cabins/page.jsx
@@ -0,0 +1,7 @@
+export default function Cabin() {
+  return (
+    <div>
+      <h1>Cabin page</h1>
+    </div>
+  );
+}
---
+++ app/loading.js
@@ -0,0 +1,7 @@
+export default function loading() {
+  return (
+    <>
+      <p>Loding data...</p>
+    </>
+  );
+}
