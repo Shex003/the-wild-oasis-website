e9ad451735e0908675010d7b969ea61c80778a1f
Shex003 <eng.osman003@gmail.com>
2024-10-04 13:36:15 +0200

    fitchingAnd DisplayingCabinList

--- app/cabins/page.jsx
+++ app/cabins/page.jsx
@@ -1,8 +1,12 @@
 import CabinCard from "@/app/_components/CabinCard";
+import { getCabins } from "../_lib/data-service";
 
-export default function Page() {
-  // CHANGE
-  const cabins = [];
+export const metadata = {
+  title: "Cabins",
+};
+
+export default async function Page() {
+  const cabins = await getCabins();
 
   return (
     <div>
