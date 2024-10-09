d138c3974aba1eb39341bf08f12000b747f8d50f
Shex003 <eng.osman003@gmail.com>
2024-10-09 12:48:22 +0200

    show more/less toggle for cabin descriptions using Client Components in Server Components

--- app/cabins/[cabinId]/page.js
+++ app/cabins/[cabinId]/page.js
@@ -1,34 +1,28 @@
-import { getCabin } from "@/app/_lib/data-service";
-import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
+import { getCabin, getCabins } from "@/app/_lib/data-service";
+import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
+import TextExpander from "@/app/_components/TextExpander";
 
-// PLACEHOLDER DATA
-// const cabin = {
-//   id: 89,
-//   name: "001",
-//   maxCapacity: 2,
-//   regularPrice: 250,
-//   discount: 0,
-//   description:
-//     "Discover the ultimate luxury getaway for couples in the cozy wooden cabin 001. Nestled in a picturesque forest, this stunning cabin offers a secluded and intimate retreat. Inside, enjoy modern high-quality wood interiors, a comfortable seating area, a fireplace and a fully-equipped kitchen. The plush king-size bed, dressed in fine linens guarantees a peaceful nights sleep. Relax in the spa-like shower and unwind on the private deck with hot tub.",
-//   image:
-//     "https://dclaevazetcjjkrzczpc.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg",
+// export const metadata = {
+//   title: "Cabin",
 // };
 
 export async function generateMetadata({ params }) {
   const { name } = await getCabin(params.cabinId);
-  return { title: `cabin ${name}` };
+  return { title: `Cabin ${name}` };
 }
 
-export async function getStaticParams() {
+export async function generateStaticParams() {
   const cabins = await getCabins();
-  const ids = cabins.map((cabin) => ({ cabinId: string(cabin.id) }));
+
+  const ids = cabins.map((cabin) => ({ cabinId: String(cabin.id) }));
+
   return ids;
 }
 
 export default async function Page({ params }) {
   const cabin = await getCabin(params.cabinId);
-  console.log("CabinId:", params.CabinId);
+
   const { id, name, maxCapacity, regularPrice, discount, image, description } =
     cabin;
 
@@ -49,7 +43,9 @@ export default async function Page({ params }) {
             Cabin {name}
           </h3>
 
-          <p className="text-lg text-primary-300 mb-10">{description}</p>
+          <p className="text-lg text-primary-300 mb-10">
+            <TextExpander>{description}</TextExpander>
+          </p>
 
           <ul className="flex flex-col gap-4 mb-7">
             <li className="flex gap-3 items-center">
