e55a5e32c7db6a4da0d4a6b4adfc9eb4b88bdd91
Shex003 <eng.osman003@gmail.com>
2024-10-13 19:40:56 +0200

    buildingACustomSignInPage-CustomSignOutButton

--- app/account/page.js
+++ app/account/page.js
@@ -1,11 +1,17 @@
+import { auth } from "../_lib/auth";
+
 export const metadata = {
-  title: "Guest account",
+  title: "Guest area",
 };
 
-export default function account() {
+export default async function Page() {
+  const session = await auth();
+
+  const firstName = session.user.name.split(" ").at(0);
+
   return (
     <h2 className="font-semibold text-2xl text-accent-400 mb-7">
-      wecome Jonas
+      Welcome, {firstName}
     </h2>
   );
 }
